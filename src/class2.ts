e9ed8f2340c042d147b5ac7ecd6ff28df11bd7d4
wch_charles <wangchanghao@aipark.com>
2021-11-19 01:59:04 +0800

    class two

--- src/class2.ts
+++ src/class2.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: wch
  * @Date: 2021-11-11 18:25:38
- * @LastEditTime: 2021-11-11 18:34:03
+ * @LastEditTime: 2021-11-16 15:49:46
  * @LastEditors: your name
  * @Description: class
  * @FilePath: \typeScript\src\class2.ts
  * 可以输入预定的版权声明、个性签名、空行等
  */
+// 无法创建抽象类实例 new Animal
 abstract class Animal {
   constructor() {
   }
@@ -23,8 +24,38 @@ class Duck extends Animal {
   }
   name: string
   sleep() {
-    console.log('sleep');
+    console.log('duck sleep');
   }
 }
+
 let duck = new Duck('gaga')
+duck.sleep()
 console.log(duck);
+
+class Pig extends Animal {
+  sleep() {
+    console.log('pig sleep');
+  }
+}
+let pig = new Pig()
+let animal: Animal[] = [duck, pig]
+animal.forEach(res => {
+  res.sleep()
+})
+
+class WorkFlow {
+  step1() {
+    return this
+  }
+  step2() {
+    return this
+  }
+}
+new WorkFlow().step1().step2() // 方法的链式调用
+
+class MyFlow extends WorkFlow {
+  next() {
+    return this
+  }
+}
+new MyFlow().next().step1()
